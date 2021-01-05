4a95519fa13138089404db5256de97aab16c2ca1
Rozaly92 <negrea.roza@mail.ru>
2021-01-05 21:43:52 +0200

    Initial commit

---
+++ src/Students/Main.java
@@ -0,0 +1,10 @@
+package Students;
+
+public class Main {
+    public static void main(String[] args) {
+      Student student = new Student();
+        System.out.println(student.toString());
+        Student student2 = new Student("Roza", "Negrea", 28);
+        System.out.println(student2.toString());
+    }
+}
