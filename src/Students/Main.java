9b8f1c0e0c02007f1b83c78af21bd6b0fff672a4
Rozaly92 <negrea.roza@mail.ru>
2021-01-05 21:51:28 +0200

    add first second console out

--- src/Students/Main.java
+++ src/Students/Main.java
@@ -2,8 +2,10 @@ package Students;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("The first student");
       Student student = new Student();
         System.out.println(student.toString());
+        System.out.println("The second student");
         Student student2 = new Student("Roza", "Negrea", 28);
         System.out.println(student2.toString());
     }
